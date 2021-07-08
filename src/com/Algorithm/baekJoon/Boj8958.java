4f496366ecccca6ed90f2d319c1bdb086a7fa1ae
pelexsa <pelexsa@naver.com>
2021-07-08 23:32:34 +0900

    Boj8958

---
+++ src/com/Algorithm/baekJoon/Boj8958.java
@@ -0,0 +1,33 @@
+package com.Algorithm.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bf.readLine());
+        String[] arr = new String[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = bf.readLine();
+        }
+
+        for (int i = 0; i < num; i++) {
+            int sum = 0;
+            int cnt = 0;
+
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+                sum += cnt;
+            }
+            System.out.println(sum);
+        }
+    }
+}
