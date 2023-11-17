07d38c043c0c947a755a2e69715dc29bd3d77ee1
wwl.hmh <hmh@winwinlab.co.kr>
2023-11-17 14:57:52 +0900

    [add] 방 번호 - 바킹독님 풀이

--- src/com/Algorithm/java/baekJoon/barkingDog/array0x03/Boj3273.java
+++ src/com/Algorithm/java/baekJoon/barkingDog/array0x03/Boj3273.java
@@ -6,20 +6,38 @@ import java.util.StringTokenizer;
 public class Boj3273 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine()), idx = 0, ans = 0;
+		int[] a = new int[1000001];
+		boolean[] occur = new boolean[2000001];
+
+		int ans = 0, n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int[] arr = new int[n];
-		while (st.hasMoreTokens())
-			arr[idx++] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) a[i] = Integer.parseInt(st.nextToken());
 		int x = Integer.parseInt(br.readLine());
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] + arr[j] == x) {
-					ans++;
-					break;
-				}
-			}
+
+		for (int i = 0; i < n; i++) {
+			if (x - a[i] > 0 && occur[x - a[i]]) ans++;
+			occur[a[i]] = true;
 		}
 		System.out.println(ans);
 	}
+
+	//내 풀이
+	//	public static void main(String[] args) throws Exception {
+	//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	//		int n = Integer.parseInt(br.readLine()), idx = 0, ans = 0;
+	//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	//		int[] arr = new int[n];
+	//		while (st.hasMoreTokens())
+	//			arr[idx++] = Integer.parseInt(st.nextToken());
+	//		int x = Integer.parseInt(br.readLine());
+	//		for (int i = 0; i < arr.length - 1; i++) {
+	//			for (int j = i + 1; j < arr.length; j++) {
+	//				if (arr[i] + arr[j] == x) {
+	//					ans++;
+	//					break;
+	//				}
+	//			}
+	//		}
+	//		System.out.println(ans);
+	//	}
 }
