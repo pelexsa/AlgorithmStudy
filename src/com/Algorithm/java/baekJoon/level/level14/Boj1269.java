03ee18427dd57c0bf9df1c6e20f36c9bc5bb48a0
wwl.hmh <hmh@winwinlab.co.kr>
2023-11-17 16:17:18 +0900

    [add] 대칭 차집합

--- src/com/Algorithm/java/baekJoon/level/level14/Boj1269.java
+++ src/com/Algorithm/java/baekJoon/level/level14/Boj1269.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Boj1269 {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
