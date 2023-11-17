08f5f3d7957aaa7df434be70e6c8eac7bcc89814
wwl.hmh <hmh@winwinlab.co.kr>
2023-11-17 16:48:39 +0900

    [change] 패키지명

--- src/com/Algorithm/java/baekJoon/level/집합과맵/Boj10815.java
+++ src/com/Algorithm/java/baekJoon/level/집합과맵/Boj10815.java
@@ -1,25 +1,29 @@
-package com.Algorithm.java.baekJoon.level.level14;
+package com.Algorithm.java.baekJoon.level.집합과맵;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Boj10816 {
+public class Boj10815 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
         br.readLine();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        Map<Integer, Integer> map = new HashMap<>();
         while (st.hasMoreTokens()) {
             int t = Integer.parseInt(st.nextToken());
-            map.put(t, map.getOrDefault(t, 0) + 1);
+            map.put(t, map.getOrDefault(t + 1, 1));
         }
 
         br.readLine();
-        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine(), " ");
         while (st.hasMoreTokens()) {
-            sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append(' ');
+            if(map.containsKey(Integer.parseInt(st.nextToken())))
+                sb.append(1).append(' ');
+            else
+                sb.append(0).append(' ');
         }
 
         System.out.println(sb);
--- src/com/Algorithm/java/baekJoon/level/집합과맵/Boj1269.java
+++ src/com/Algorithm/java/baekJoon/level/집합과맵/Boj1269.java
@@ -1,4 +1,4 @@
-package com.Algorithm.java.baekJoon.level.level14;
+package com.Algorithm.java.baekJoon.level.집합과맵;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
