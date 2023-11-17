44e6650a77ae991d4e1774c3483fa512580e81dc
wwl.hmh <hmh@winwinlab.co.kr>
2023-11-17 14:40:49 +0900

    [add] 두 수의 합 - 내 풀이

---
+++ src/com/Algorithm/java/baekJoon/level/level01/Boj3273.java
@@ -0,0 +1,25 @@
+package com.Algorithm.java.baekJoon.level.level01;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj3273 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()), idx = 0, ans = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[n];
+		while (st.hasMoreTokens())
+			arr[idx++] = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(br.readLine());
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == x) {
+					ans++;
+					break;
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}
