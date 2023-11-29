14d10b6f676ed12b384159ca120391c084109e92
wwl.hmh <hmh@winwinlab.co.kr>
2023-11-29 16:40:19 +0900

    [add] Strfry

--- src/com/Algorithm/java/baekJoon/level/배열/Boj11328.java
+++ src/com/Algorithm/java/baekJoon/level/배열/Boj11328.java
@@ -5,13 +5,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj11328 {
-    /**
-     *
-     *
-     */
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++)
+            System.out.println(strfry(br.readLine()));
+    }
 
-        br.close();
+    static String strfry(String input) {
+        StringTokenizer st = new StringTokenizer(input, " ");
+        String s1 = st.nextToken(), s2 = st.nextToken();
+        int[][] arr = new int[2][26];
+
+        for(int i=0; i<s1.length(); i++)
+            arr[0][s1.charAt(i) - 'a']++;
+
+        for(int i=0; i<s2.length(); i++)
+            arr[1][s2.charAt(i) - 'a']++;
+
+        for(int i=0; i<26; i++)
+            if(arr[0][i] != arr[1][i])
+                return "Impossible";
+
+        return "Possible";
     }
 }
