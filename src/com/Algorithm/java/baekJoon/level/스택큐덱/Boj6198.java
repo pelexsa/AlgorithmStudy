340df630f7be91a683e06fc830dad0291acaa8ef
wwl.hmh <hmh@winwinlab.co.kr>
2024-04-02 16:23:29 +0900

    옥상 정원 꾸미기

---
+++ src/com/Algorithm/java/baekJoon/level/스택큐덱/Boj6198.java
@@ -0,0 +1,28 @@
+package com.Algorithm.java.baekJoon.level.스택큐덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Boj6198 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long result = 0;
+		Stack<Integer> s = new Stack<>();
+
+		for (int i = 0; i < n; i++) {
+			int c = Integer.parseInt(br.readLine());
+			while (!s.isEmpty() && s.peek() <= c) {
+				s.pop();
+			}
+
+			if (!s.isEmpty()) {
+				result += s.size();
+			}
+			s.push(c);
+		}
+		System.out.println(result);
+	}
+}
